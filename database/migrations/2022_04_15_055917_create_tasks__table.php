d3482afa7699a2a7fd9fd597affe151f4a68479e
Webex <Webex@example.com>
2022-04-15 10:17:24 +0400

    ch

--- database/migrations/2022_04_15_055917_create_tasks__table.php
+++ database/migrations/2022_04_15_055917_create_tasks__table.php
@@ -13,18 +13,17 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('tasks_', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('lesson_id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->integer('phone_number')->nullable();
-            $table->text('description')->nullable();
-            $table->string('avatar')->nullable();
-            $table->string('criterion')->nullable();
+            $table->text('description');
             $table->timestamps();
+
+            $table->foreign('lesson_id')
+            ->references('id')
+            ->on('lessons')
+            ->onDelete('cascade');
         });
     }
 
@@ -35,6 +34,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('tasks_');
     }
 };
--- database/migrations/2022_04_15_060135_create_test_table.php
+++ database/migrations/2022_04_15_060135_create_test_table.php
@@ -13,18 +13,18 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('tests', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('lesson_id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->integer('phone_number')->nullable();
-            $table->text('description')->nullable();
-            $table->string('avatar')->nullable();
-            $table->string('criterion')->nullable();
+            $table->text('description');
+            $table->time('time_limit');
             $table->timestamps();
+
+            $table->foreign('lesson_id')
+            ->references('id')
+            ->on('lessons')
+            ->onDelete('cascade');
         });
     }
 
@@ -35,6 +35,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('test');
     }
 };
